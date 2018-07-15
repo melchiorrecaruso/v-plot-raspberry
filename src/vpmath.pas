79518d8f0b59a81576a040150f5ffa8c81839875
Melchiorre Caruso <melchiorrecaruso@gmail.com>
2018-07-15 16:37:25 +0200

    New units

---
+++ src/vpmath.pas
@@ -0,0 +1,196 @@
+{
+  Description: vPlot common unit.
+
+  Copyright (C) 2017-2018 Melchiorre Caruso <melchiorrecaruso@gmail.com>
+
+  This source is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This code is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  A copy of the GNU General Public License is available on the World Wide Web
+  at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
+  to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+  MA 02111-1307, USA.
+}
+
+unit vpmath;
+
+{$mode objfpc}
+
+interface
+
+uses
+  classes, sysutils;
+
+type
+  tvppoint = packed record
+    x: double;
+    y: double;
+  end;
+  pvppoint = ^tvppoint;
+
+  tvpline = packed record
+    a: double;
+    b: double;
+    c: double;
+  end;
+  pvpline = ^tvpline;
+
+  tvpcircle = packed record
+    a: double;
+    b: double;
+    c: double;
+  end;
+  pvpcircle = ^tvpcircle;
+
+
+function translate_point(const p, cc: tvppoint): tvppoint;
+function line_by_two_points(const p0, p1: tvppoint): tvpline;
+function rotate_point(const p: tvppoint; const alpha: double): tvppoint;
+function distance_between_two_points(const p0, p1: tvppoint): double;
+function intersection_of_two_lines(const l0, l1: tvpline): tvppoint;
+function circle_by_three_points(const p0, p1, p2: tvppoint): tvpcircle;
+function circle_by_center_and_radius(const cc: tvppoint; radius: double): tvpcircle;
+
+
+function intersection_of_two_circles(const c0, c1: tvpcircle; var p1, p2: tvppoint): longint;
+
+function get_line_angle(const line: tvpline): double;
+
+
+
+
+implementation
+
+uses
+  math;
+
+function translate_point(const p, cc: tvppoint): tvppoint;
+begin
+  result.x := p.x + cc.x;
+  result.y := p.y + cc.y;
+end;
+
+function line_by_two_points(const p0, p1: tvppoint): tvpline;
+begin
+  result.a :=  p1.y - p0.y;
+  result.b :=  p0.x - p1.x;
+  result.c := (p1.x - p0.x) * p0.y -(p1.y - p0.y) * p0.x;
+end;
+
+function rotate_point(const p: tvppoint; const alpha: double): tvppoint;
+var
+  sinus, cosinus: double;
+begin
+  sincos(alpha, sinus, cosinus);
+  result.x := p.x * cosinus - p.y *   sinus;
+  result.y := p.x *   sinus + p.y * cosinus;
+end;
+
+function distance_between_two_points(const p0, p1: tvppoint): double;
+begin
+  result := sqrt(sqr(p1.x - p0.x) + sqr(p1.y - p0.y));
+end;
+
+function intersection_of_two_lines(const l0, l1: tvpline): tvppoint;
+begin
+  if (l0.a * l1.b) <> (l0.b * l1.a) then
+  begin
+    result.x := (-l0.c * l1.b + l0.b * l1.c) / (l0.a * l1.b - l0.b * l1.a);
+    result.y := (-l0.c - l0.a * result.x) / (l0.b);
+  end else
+    raise exception.create('intersection_of_two_lines exception');
+end;
+
+function circle_by_three_points(const p0, p1, p2: tvppoint): tvpcircle;
+begin
+   raise exception.create('circle_by_three_points exception');
+end;
+
+function circle_by_center_and_radius(const cc: tvppoint; radius: double): tvpcircle;
+begin
+  result.a :=  -2*cc.x;
+  result.b :=  -2*cc.y;
+  result.c := sqr(cc.x)+
+              sqr(cc.y)-
+              sqr(radius);
+end;
+
+function intersection_of_two_circles(const c0, c1: tvpcircle; var p1, p2: tvppoint): longint;
+var
+  aa, bb, cc, dd: double;
+begin
+  aa := 1+sqr((c0.b-c1.b)/(c1.a-c0.a));
+  bb := 2*(c0.b-c1.b)/(c1.a-c0.a)*(c0.c-c1.c)/(c1.a-c0.a)+c1.a*(c0.b-c1.b)/(c1.a-c0.a)+c1.b;
+  cc := c1.a*(c0.c-c1.c)/(c1.a-c0.a)+sqr((c0.c-c1.c)/(c1.a-c0.a))+c1.c;
+  dd := sqr(bb)-4*aa*cc;
+
+  if dd > 0 then
+  begin
+    result := 2;
+    p1.y   := (-bb-sqrt(dd))/(2*aa);
+    p2.y   := (-bb+sqrt(dd))/(2*aa);
+    p1.x   := ((c0.c-c1.c)+(c0.b-c1.b)*p1.y)/(c1.a-c0.a);
+    p2.x   := ((c0.c-c1.c)+(c0.b-c1.b)*p2.y)/(c1.a-c0.a);
+  end else
+    if dd = 0 then
+    begin
+      result := 1;
+      p1.y   := -bb;
+      p1.x   := ((c0.c-c1.c)+(c0.b-c1.b)*p1.y)/(c1.a-c0.a);
+      p2     := p1;
+    end else
+      result := 0;
+end;
+
+function intersection_of_line_and_circles(const l0: tvpline; const c1: tvpcircle; var p1, p2: tvppoint): longint;
+var
+  aa, bb, cc, dd: double;
+begin
+  aa := 1+sqr((l0.b-c1.b)/(c1.a-l0.a));
+  bb := 2*(l0.b-c1.b)/(c1.a-l0.a)*(l0.c-c1.c)/(c1.a-l0.a)+c1.a*(l0.b-c1.b)/(c1.a-l0.a)+c1.b;
+  cc := c1.a*(l0.c-c1.c)/(c1.a-l0.a)+sqr((l0.c-c1.c)/(c1.a-l0.a))+c1.c;
+  dd := sqr(bb)-4*aa*cc;
+
+  if dd > 0 then
+  begin
+    result := 2;
+    p1.y   := (-bb-sqrt(dd))/(2*aa);
+    p2.y   := (-bb+sqrt(dd))/(2*aa);
+    p1.x   := ((l0.c-c1.c)+(l0.b-c1.b)*p1.y)/(c1.a-l0.a);
+    p2.x   := ((l0.c-c1.c)+(l0.b-c1.b)*p2.y)/(c1.a-l0.a);
+  end else
+    if dd = 0 then
+    begin
+      result := 1;
+      p1.y   := -bb;
+      p1.x   := ((l0.c-c1.c)+(l0.b-c1.b)*p1.y)/(c1.a-l0.a);
+      p2     := p1;
+    end else
+      result := 0;
+end;
+
+function get_line_angle(const line: tvpline): double;
+begin
+  if line.b = 0 then
+  begin
+    if line.a > 0 then
+      result := +pi / 2
+    else
+      result := -pi / 2;
+  end else
+    result := arctan2(line.a, -line.b);
+end;
+
+
+
+
+
+end.
+
---
+++ src/vpwave.pas
@@ -0,0 +1,198 @@
+
+{
+  Description: vPlot wave class.
+
+  Copyright (C) 2017-2018 Melchiorre Caruso <melchiorrecaruso@gmail.com>
+
+  This source is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This code is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  A copy of the GNU General Public License is available on the World Wide Web
+  at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
+  to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+  MA 02111-1307, USA.
+}
+
+unit vpwave;
+
+{$mode objfpc}
+
+interface
+
+uses
+  classes, sysutils, vpcommon, vpmath;
+
+type
+  tdegres = 0..10;
+
+  tpolynome = packed record
+    coefs: array[tdegres] of double;
+    deg:   tdegres;
+  end;
+
+  twavemesh = array[0..8] of tvppoint;
+
+  twave = class
+  private
+    lax, lay: tpolynome;
+    lbx, lby: tpolynome;
+    lcx, lcy: tpolynome;
+  public
+    constructor create(xmax, ymax: double; const mesh: twavemesh);
+    destructor destroy; override;
+    function update(const p: tvppoint): tvppoint;
+    procedure test;
+  end;
+
+  function polyeval(const apoly: tpolynome; x: double): double;
+
+
+var
+  wave: twave = nil;
+
+implementation
+
+uses
+  matrix;
+
+// polynomial evaluation
+
+function polyeval(const apoly: tpolynome; x: double): double;
+var
+  i: tdegres;
+begin
+  with apoly do
+  begin
+    result := 0;
+    for i := deg downto low(coefs) do
+      result := result * x + coefs[i];
+  end;
+end;
+
+// twave
+
+constructor twave.create(xmax, ymax: double; const mesh: twavemesh);
+var
+   a, aa: tvector3_double;
+   b, bb: tvector3_double;
+   c, cc: tvector3_double;
+  dy: tvector3_double;
+  dx: tvector3_double;
+   y: tmatrix3_double;
+   x: tmatrix3_double;
+   p: tvppoint;
+begin
+  inherited create;
+  xmax := abs(xmax);
+  ymax := abs(ymax);
+
+  x.init(1, -xmax, sqr(-xmax), 1, 0, 0, 1, +xmax, sqr(+xmax));
+  y.init(1, +ymax, sqr(+ymax), 1, 0, 0, 1, -ymax, sqr(-ymax));
+  x := x.inverse(x.determinant);
+  y := y.inverse(y.determinant);
+
+  // calculate y-mirror
+  dy.init(mesh[0].y, mesh[1].y, mesh[2].y);   a := x * dy;
+  dy.init(mesh[3].y, mesh[4].y, mesh[5].y);   b := x * dy;
+  dy.init(mesh[6].y, mesh[7].y, mesh[8].y);   c := x * dy;
+
+  dx.init(a.data[0], b.data[0], c.data[0]);  cc := y * dx;
+  dx.init(a.data[1], b.data[1], c.data[1]);  bb := y * dx;
+  dx.init(a.data[2], b.data[2], c.data[2]);  aa := y * dx;
+
+  lay.deg :=2;
+  lay.coefs[2] := aa.data[2];
+  lay.coefs[1] := aa.data[1];
+  lay.coefs[0] := aa.data[0];
+
+  lby.deg :=2;
+  lby.coefs[2] := bb.data[2];
+  lby.coefs[1] := bb.data[1];
+  lby.coefs[0] := bb.data[0];
+
+  lcy.deg :=2;
+  lcy.coefs[2] := cc.data[2];
+  lcy.coefs[1] := cc.data[1];
+  lcy.coefs[0] := cc.data[0];
+
+  // calculate x-mirror
+  dx.init(mesh[0].x, mesh[3].x, mesh[6].x);   a := y * dx;
+  dx.init(mesh[1].x, mesh[4].x, mesh[7].x);   b := y * dx;
+  dx.init(mesh[2].x, mesh[5].x, mesh[8].x);   c := y * dx;
+
+  dy.init(a.data[0], b.data[0], c.data[0]);  cc := x * dy;
+  dy.init(a.data[1], b.data[1], c.data[1]);  bb := x * dy;
+  dy.init(a.data[2], b.data[2], c.data[2]);  aa := x * dy;
+
+  lax.deg :=2;
+  lax.coefs[2] := aa.data[2];
+  lax.coefs[1] := aa.data[1];
+  lax.coefs[0] := aa.data[0];
+
+  lbx.deg :=2;
+  lbx.coefs[2] := bb.data[2];
+  lbx.coefs[1] := bb.data[1];
+  lbx.coefs[0] := bb.data[0];
+
+  lcx.deg :=2;
+  lcx.coefs[2] := cc.data[2];
+  lcx.coefs[1] := cc.data[1];
+  lcx.coefs[0] := cc.data[0];
+end;
+
+destructor twave.destroy;
+begin
+  inherited destroy;
+end;
+
+function twave.update(const p: tvppoint): tvppoint;
+var
+  ly, lx: tpolynome;
+begin
+  ly.deg :=2;
+  ly.coefs[2] := polyeval(lay, p.y);
+  ly.coefs[1] := polyeval(lby, p.y);
+  ly.coefs[0] := polyeval(lcy, p.y);
+
+  lx.deg :=2;
+  lx.coefs[2] := polyeval(lax, p.x);
+  lx.coefs[1] := polyeval(lbx, p.x);
+  lx.coefs[0] := polyeval(lcx, p.x);
+
+  result.x := p.x + polyeval(lx, p.y);
+  result.y := p.y + polyeval(ly, p.x);
+
+  if enabledebug then
+  begin
+    writeln(format('  WAVING::P.X    = %12.5f  P''.X = %12.5f', [p.x, result.x]));
+    writeln(format('  WAVING::P.Y    = %12.5f  P''.Y = %12.5f', [p.y, result.y]));
+  end;
+end;
+
+procedure twave.test;
+var
+  p: tvppoint;
+begin
+  if enabledebug then
+  begin
+    p.x := -594.5;  p.y := +420.5; wave.update(p);
+    p.x := +0.000;  p.y := +420.5; wave.update(p);
+    p.x := +594.5;  p.y := +420.5; wave.update(p);
+    p.x := -594.5;  p.y := +0.000; wave.update(p);
+    p.x := +0.000;  p.y := +0.000; wave.update(p);
+    p.x := +594.5;  p.y := +0.000; wave.update(p);
+    p.x := -594.5;  p.y := -420.5; wave.update(p);
+    p.x := +0.000;  p.y := -420.5; wave.update(p);
+    p.x := +594.5;  p.y := -420.5; wave.update(p);
+  end;
+end;
+
+end.
+
