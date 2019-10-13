b37ba0676c4aba4f58e7ca4c6ea82413657a28d6
Melchiorre Caruso <melchiorrecaruso@gmail.com>
2019-10-13 16:30:25 +0200

    update

--- src/setupfrm.pas
+++ src/setupfrm.pas
@@ -37,8 +37,8 @@ type
   tsetupform = class(tform)
     clearbtn: TBitBtn;
     calculatebtn: tbitbtn;
+    memo: TStringGrid;
     resultlb: TLabel;
-    resultmemo: TMemo;
     pagelb: tlabel;
     widthse: TFloatSpinEdit;
     heigthse: TFloatSpinEdit;
@@ -103,17 +103,18 @@ begin
   homexse.value   := 0;
   homeyse.value   := 0;
 
-  resultmemo.clear;
+  clearmemo(sender);
 end;
 
 procedure tsetupform.clearmemo(sender: tobject);
 begin
-  resultmemo.clear;
+  memo.clean;
 end;
 
 procedure tsetupform.calculatebtnclick(sender: tobject);
 const
-  range = 500;
+  range    = 1500;
+  rangeinc = 2;
 var
   arrp:  array [0.. 9] of tvppoint;
   arrpr: array [1 ..9] of tvppoint;
@@ -122,18 +123,18 @@ var
   t1: tvppoint;
   p, pr:  tvppoint;
 
-  l0,  ll0: vpfloat;
-  l1,  ll1: vpfloat;
-  lr0, llr0: vpfloat;
-  lr1, llr1: vpfloat;
-  dl0, dl1: vpfloat;
+  l0,  li0: vpfloat;
+  l1,  li1: vpfloat;
+  lr0, lri0: vpfloat;
+  lr1, lri1: vpfloat;
+  dl0,  dl1: vpfloat;
+
      i: longint;
   x, y: longint;
 begin
   // real machine setting
   t0.x := setting.point0.x + pnt0xse.value;
   t0.y := setting.point0.y + pnt0yse.value;
-
   t1.x := setting.point1.x + pnt1xse.value;
   t1.y := setting.point1.y + pnt1yse.value;
 
@@ -175,61 +176,60 @@ begin
   arrpr[8].x := -1; arrpr[8].y := -1;
   arrpr[9].x := -1; arrpr[9].y := -1;
 
-  // run
+  // run ...
   for i := 1 to 9 do
   begin
     // ideal machine
     calc_(arrp[0], l0,   l1);
-    calc_(arrp[i], ll0, ll1);
+    calc_(arrp[i], li0, li1);
 
     // real machine
     p.x := arrp[0].x + homexse.value;
     p.y := arrp[0].y + homeyse.value;
     lr0 := calc_l0(p, t0, setting.m0radius + radius0se.value);
     lr1 := calc_l1(p, t1, setting.m1radius + radius1se.value);
-
-    llr0 := lr0 + (ll0-l0)/setting.m0radius*(setting.m0radius+ratio0se.value);
-    llr1 := lr1 + (ll1-l1)/setting.m1radius*(setting.m1radius+ratio1se.value);
+    lri0 := lr0 + (li0-l0)/setting.m0ratio*(setting.m0ratio+ratio0se.value);
+    lri1 := lr1 + (li1-l1)/setting.m1ratio*(setting.m1ratio+ratio1se.value);
 
     y := -range;
     while y <= range do
     begin
-      x := -range;
+      x  := -range;
       while x <= range do
       begin
         pr.x := arrp[i].x + x/100;
         pr.y := arrp[i].y + y/100;
 
-        dl0 := abs(calc_l0(pr, t0, setting.m0radius + radius0se.value) - llr0);
-        dl1 := abs(calc_l1(pr, t1, setting.m1radius + radius1se.value) - llr1);
+        dl0 := abs(calc_l0(pr, t0, setting.m0radius + radius0se.value) - lri0);
+        dl1 := abs(calc_l1(pr, t1, setting.m1radius + radius1se.value) - lri1);
 
-        if (dl0 < 0.02) and (dl1 < 0.02) then
+        if (dl0 < 0.05) and (dl1 < 0.05) then
         begin
           arrpr[i] := pr;
         end;
-
-        inc(x);
+        inc(x, rangeinc);
       end;
-      inc(y);
+      inc(y, rangeinc);
     end;
   end;
 
-  resultmemo.clear;
-  resultmemo.append('');
-  resultmemo.append(format('    ( X%1.1f ,  Y%1.1f )  ( X%1.1f ,  Y%1.1f )  ( X%1.1f ,  Y%1.1f )',
-    [arrpr[1].x-arrp[1].x, arrpr[1].y-arrp[1].y,
-     arrpr[2].x-arrp[2].x, arrpr[2].y-arrp[2].y,
-     arrpr[3].x-arrp[3].x, arrpr[3].y-arrp[3].y]));
+  p := arrpr[5];
+  for i := 1 to 9 do
+  begin
+    arrpr[i].x := arrpr[i].x - (p.x -arrp[5].x) + arrp[i].x - c.x;
+    arrpr[i].y := arrpr[i].y - (p.y -arrp[5].y) + arrp[i].y - c.y;
+  end;
+  memo.cells[0, 0] := format('(X%3.1f, Y%3.1f)', [arrpr[1].x-arrp[1].x, arrpr[1].y-arrp[1].y]);
+  memo.cells[1, 0] := format('(X%3.1f, Y%3.1f)', [arrpr[2].x-arrp[2].x, arrpr[2].y-arrp[2].y]);
+  memo.cells[2, 0] := format('(X%3.1f, Y%3.1f)', [arrpr[3].x-arrp[3].x, arrpr[3].y-arrp[3].y]);
 
-  resultmemo.append(format('    ( X%1.1f ,  Y%1.1f )  ( X%1.1f ,  Y%1.1f )  ( X%1.1f ,  Y%1.1f )',
-    [arrpr[4].x-arrp[4].x, arrpr[4].y-arrp[4].y,
-     arrpr[5].x-arrp[5].x, arrpr[5].y-arrp[5].y,
-     arrpr[6].x-arrp[6].x, arrpr[6].y-arrp[6].y]));
+  memo.cells[0, 1] := format('(X%3.1f, Y%3.1f)', [arrpr[4].x-arrp[4].x, arrpr[4].y-arrp[4].y]);
+  memo.cells[1, 1] := format('(X%3.1f, Y%3.1f)', [arrpr[5].x-arrp[5].x, arrpr[5].y-arrp[5].y]);
+  memo.cells[2, 1] := format('(X%3.1f, Y%3.1f)', [arrpr[6].x-arrp[6].x, arrpr[6].y-arrp[6].y]);
 
-  resultmemo.append(format('    ( X%1.1f ,  Y%1.1f )  ( X%1.1f ,  Y%1.1f )  ( X%1.1f ,  Y%1.1f )',
-    [arrpr[7].x-arrp[7].x, arrpr[7].y-arrp[7].y,
-     arrpr[8].x-arrp[8].x, arrpr[8].y-arrp[8].y,
-     arrpr[9].x-arrp[9].x, arrpr[9].y-arrp[9].y]));
+  memo.cells[0, 2] := format('(X%3.1f, Y%3.1f)', [arrpr[7].x-arrp[7].x, arrpr[7].y-arrp[7].y]);
+  memo.cells[1, 2] := format('(X%3.1f, Y%3.1f)', [arrpr[8].x-arrp[8].x, arrpr[8].y-arrp[8].y]);
+  memo.cells[2, 2] := format('(X%3.1f, Y%3.1f)', [arrpr[9].x-arrp[9].x, arrpr[9].y-arrp[9].y]);
 end;
 
 end.
